f4091388ec27128bcc45a5957ec2a22c1fce0b75
David Ewan Campbell <ewan.david@gmail.com>
2022-03-22 17:30:29 +0000

    release bike test added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/docking_station.rb'
 
 describe DockingStation do
-  
+  it 'releases bike' do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to eq('bike released')
+  end
 end
