c3f1d76e006db3948e1f6bd9ad24f89569f72d9f
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-22 17:15:10 +0000

    Handover to pairing partner- minor changes to spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,5 @@
 require_relative '../lib/docking_station.rb'
-<<<<<<< HEAD
 
-describe 'docking_station' do
-
-=======
 describe DockingStation do
   
->>>>>>> c52a64be8f0e37a127ee934251d5b9b67065e2cb
 end
